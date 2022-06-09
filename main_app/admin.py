27c4fade294a5d61fa4823d0939172060fceebe9
SaidNivek <kevin.vi.dias@gmail.com>
2022-06-08 20:00:56 -0400

    Working on search route for seeding new games into the DB

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import TrendingGame, Game
+from .models import TrendingGame, Game, Collection
 
 # Register your models here.
 admin.site.register(TrendingGame)
 admin.site.register(Game)
+admin.site.register(Collection)
--- main_app/views.py
+++ main_app/views.py
@@ -35,28 +35,49 @@ class GameDetail(DetailView):
         return context   
 
 class SearchGame(TemplateView):
-    template_name = "search_game.html"
+    template_name = "search.html"
 
-    def get_games(request):
-        all_games = {}
-        name = request.GET['name']
-        url = 'https://api.geekdo.com/xmlapi2/search?query=' + name
-        req = requests.get(url)
-        games = ET.fromstring(req.content)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name") 
+        # if name != None:
+        #     context["games"] = Game.objects.filter(name__icontains=name)
+        #     context["header"] = f"Searching for {name}"
+        # else:
+        #     context["header"] = "Search for a game"
+        # return context
 
-        for i in games.findall('item'):
-            the_game = Game(
-                bgg_id = i.get('id'),                  
-                name = the_game.find('name').get('value'),
-                thumbnail = the_game.find('thumbnail').text,
-                year_published = the_game.find('yearpublished').get('value'),
-                img = the_game.find('image').text,
-                description = the_game.find('description').text.replace('&#10;', ' '),
-                min_players = the_game.find('minplayers').get('value'),
-                max_players = the_game.find('maxplayers').get('value'),
-                min_playtime = the_game.find('minplaytime').get('value'),
-                max_playtime = the_game.find('maxplaytime').get('value'),
-                min_age = the_game.find('minage').get('value'),
-                slug = i.get('id')
-            )
-            the_game.save()
+        if name != None:
+            if len(name) > 4:
+                url = 'https://api.geekdo.com/xmlapi2/search?query=' + name
+                req = requests.get(url)
+                games = ET.fromstring(req.content)
+                print(games)
+                context["header"] = f"Searching for {name}"
+            elif len(name) <= 4:
+                print("did not hit get route")
+                context["header"] = "Search with more than 4 letters"
+            else:
+                context["header"] = "Search for a game"
+            return context
+        else:
+            context["header"] = "Search for a game"
+        return context
+
+                # for i in games.findall('name'):
+                #     the_game = Game(
+                #         bgg_id = i.get('id'),                  
+                #         name = the_game.find('name').get('value'),
+                #         thumbnail = the_game.find('thumbnail').text,
+                #         year_published = the_game.find('yearpublished').get('value'),
+                #         img = the_game.find('image').text,
+                #         description = the_game.find('description').text.replace('&#10;', '\n'),
+                #         min_players = the_game.find('minplayers').get('value'),
+                #         max_players = the_game.find('maxplayers').get('value'),
+                #         min_playtime = the_game.find('minplaytime').get('value'),
+                #         max_playtime = the_game.find('maxplaytime').get('value'),
+                #         min_age = the_game.find('minage').get('value'),
+                #         slug = i.get('id')
+                #     )
+                    
+                    # the_game.save()
